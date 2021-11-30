d928c26b1131f9a5cc0d192732f441133e8ffa5f
Thabisan <thabisan@hotmail.fr>
2021-11-30 19:27:46 +0100

    updated test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,7 +10,6 @@ def test_default_route():
 def test_mean_route():
     response = app.test_client().get('/the_mean?the_list=1,2')
     assert response.status_code == 200
-    assert response.data == default_route_text.encode('UTF-8')
     
 def test_the_mean_output():
     response = app.test_client().get('/the_mean?the_list=1,2,3')
